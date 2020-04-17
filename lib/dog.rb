8c2df5dfd6095198f49243903bf88c2953e95a45
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-17 22:53:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
  
-  def initialize
-    @dogname = dogname
-  end
-
-  def name=(dogname)
-    @dogname
+  def initialize(name)
+    @name = name
   end
  
+  def name
+    @name
+  end
+  
 end
